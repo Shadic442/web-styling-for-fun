af1b450d1f608a0bac54fa433f8af8e67bb1eb43
Shadic442 <56701897+Shadic442@users.noreply.github.com>
2024-09-24 21:45:57 -0400

    mouse click events
    added events on the press start text
    on hover the text shadow appears
    when the mouse button is press and held the text shadow disappears
    on the release of the mouse button the text shadow appears

--- jqueryFunction.js
+++ jqueryFunction.js
@@ -3,7 +3,16 @@
  * on the element with the id #pressstart
  */
 $(document).ready(function () {
+  /* on hover the text shadow appears */
   $("#pressstart").hover(function () {
+    $(this).toggleClass("classWithShadow");
+  });
+  /* when the mouse button is press and held the text shadow disappears */
+  $("#pressstart").on("mousedown", function () {
+    $(this).toggleClass("classWithShadow");
+  });
+  /* on the release of the mouse button the text shadow appears */
+  $("#pressstart").on("mouseup", function () {
     $(this).toggleClass("classWithShadow");
   });
 });
